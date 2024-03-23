c5df6966dc271da688301dda23524b49cfc1fe38
Yakobus Iryanto Prasethio <yakobusiryantoprasethio@gmail.com>
2024-03-23 14:36:10 +0700

    chore: push broken typescript cipher

--- src/lib/blockmaniputils.ts
+++ src/lib/blockmaniputils.ts
@@ -1,143 +1,148 @@
 /* Convert String to Uint8Array*/
 export function stringTo128BitUint8Array(str: string): Uint8Array {
-    str = checkAndModifyPlaintext(str)
-    const encoder = new TextEncoder();
-    const bytes = encoder.encode(str);
-    
-    return bytes
+  str = checkAndModifyPlaintext(str);
+  const encoder = new TextEncoder();
+  const bytes = encoder.encode(str);
+
+  return bytes;
 }
 
 /* Convert Binary String to Uint8Array */
 export function binaryStringToUint8Array(binaryString: string) {
-    binaryString = checkAndModifyBinaryString(binaryString)
-    const numElements = Math.ceil(binaryString.length / 8);
-    const uint8Array = new Uint8Array(numElements);
-    for (let i = 0; i < numElements; i++) {
-        uint8Array[i] = parseInt(binaryString.slice(i * 8, (i + 1) * 8), 2);
-    }
-    return uint8Array;
+  binaryString = checkAndModifyBinaryString(binaryString);
+  const numElements = Math.ceil(binaryString.length / 8);
+  const uint8Array = new Uint8Array(numElements);
+  for (let i = 0; i < numElements; i++) {
+    uint8Array[i] = parseInt(binaryString.slice(i * 8, (i + 1) * 8), 2);
+  }
+  return uint8Array;
 }
 
 /* Pads the plaintext to be a multiple of 16 bytes (128 bits each block) */
 export function checkAndModifyPlaintext(plaintext: string): string {
-    var endPlaintext = plaintext
-    if (plaintext.length % 16 != 0) {
-        // pad the plaintext
-        for (let i = plaintext.length; i % 16 != 0; i++) {
-            endPlaintext += " "
-        }
+  let endPlaintext = plaintext;
+  if (plaintext.length % 16 != 0) {
+    // pad the plaintext
+    for (let i = plaintext.length; i % 16 != 0; i++) {
+      endPlaintext += " ";
     }
-    return endPlaintext
+  }
+  return endPlaintext;
 }
 
 /* Pads the binary string to be a multiple of 16 bytes (128 bits each block) */
 export function checkAndModifyBinaryString(binaryString: string): string {
-    var endBinaryString = binaryString
-    if (binaryString.length % 128 != 0) {
-        // pad the binary string
-        for (let i = binaryString.length; i % 128 != 0; i++) {
-            endBinaryString += "0"
-        }
+  let endBinaryString = binaryString;
+  if (binaryString.length % 128 != 0) {
+    // pad the binary string
+    for (let i = binaryString.length; i % 128 != 0; i++) {
+      endBinaryString += "0";
     }
-    return endBinaryString
+  }
+  return endBinaryString;
 }
 
 export function checkAndModifyBinary(binary: Uint8Array) {
-    var endBinary = binary
-    if (binary.length % 16 != 0) {
-        // pad the binary
-        endBinary = new Uint8Array(Math.ceil(binary.length / 16) * 16);
-        endBinary.set(binary)
-    }
-    return endBinary
+  let endBinary = binary;
+  if (binary.length % 16 != 0) {
+    // pad the binary
+    endBinary = new Uint8Array(Math.ceil(binary.length / 16) * 16);
+    endBinary.set(binary);
+  }
+  return endBinary;
 }
 
 /* Repeats or truncates the key to 16 bytes (128 bits each block) */
 export function checkAndModifyKey(key: string) {
-    var endKey = key
-    if (key.length < 16) {
-        // repeat the key
-        for (let i = key.length; i < 16; i++) {
-            endKey += key[i % key.length]
-        }
-    } else if (key.length > 16) {
-        // truncate the key
-        endKey = key.slice(0, 16)
-    } 
-    return endKey
+  let endKey = key;
+  if (key.length < 16) {
+    // repeat the key
+    for (let i = key.length; i < 16; i++) {
+      endKey += key[i % key.length];
+    }
+  } else if (key.length > 16) {
+    // truncate the key
+    endKey = key.slice(0, 16);
+  }
+  return endKey;
 }
 
 /* Shifts the bits of a 128-bit block to the left */
-export function shiftLeft128(bits128: Uint8Array): void{
-    const carry = bits128[0] & 0x80 ? 1 : 0; // Capture the carry bit from the leftmost byte
-    for (let i = 0; i < bits128.length - 1; i++) {
-        bits128[i] = (bits128[i] << 1) | (bits128[i + 1] & 0x80 ? 1 : 0); // Shift left and add the carry bit from the next byte
-    }
-    bits128[bits128.length - 1] = (bits128[bits128.length - 1] << 1) | carry; // Shift left and add the captured carry bit
+export function shiftLeft128(bits128: Uint8Array): void {
+  const carry = bits128[0] & 0x80 ? 1 : 0; // Capture the carry bit from the leftmost byte
+  for (let i = 0; i < bits128.length - 1; i++) {
+    bits128[i] = (bits128[i] << 1) | (bits128[i + 1] & 0x80 ? 1 : 0); // Shift left and add the carry bit from the next byte
+  }
+  bits128[bits128.length - 1] = (bits128[bits128.length - 1] << 1) | carry; // Shift left and add the captured carry bit
 }
 
 /* Shifts the bits of a 128-bit block to the right*/
-export function shiftRight128(bits128: Uint8Array): void{
-    const n = bits128.length;
-    const carry = bits128[n - 1] & 0x01; // Capture the carry bit from the rightmost bit of the last byte
-    for (let i = n - 1; i > 0; i--) {
-        bits128[i] = (bits128[i] >> 1) | ((bits128[i - 1] & 0x01) << 7); // Shift right and add the carry bit from the previous byte
-    }
-    bits128[0] = bits128[0] >> 1; // Shift right the leftmost byte
-    bits128[0] |= carry << 7; // Add the captured carry bit to the rightmost bit of the leftmost byte
+export function shiftRight128(bits128: Uint8Array): void {
+  const n = bits128.length;
+  const carry = bits128[n - 1] & 0x01; // Capture the carry bit from the rightmost bit of the last byte
+  for (let i = n - 1; i > 0; i--) {
+    bits128[i] = (bits128[i] >> 1) | ((bits128[i - 1] & 0x01) << 7); // Shift right and add the carry bit from the previous byte
+  }
+  bits128[0] = bits128[0] >> 1; // Shift right the leftmost byte
+  bits128[0] |= carry << 7; // Add the captured carry bit to the rightmost bit of the leftmost byte
 }
 
 /* XOR Operation 128-bit block */
 export function xor(block: Uint8Array, element: Uint8Array) {
-    const block_temp = new Uint8Array(block.length)
-    block_temp.set(block)
-    for (let i = 0; i < block.length; i++) {
-        block_temp[i] = block_temp[i] ^ element[i]
-    }
-    return block_temp
+  const block_temp = new Uint8Array(block.length);
+  block_temp.set(block);
+  for (let i = 0; i < block.length; i++) {
+    block_temp[i] = block_temp[i] ^ element[i];
+  }
+  return block_temp;
 }
 
 /* Encrypts a 128-bit block using a 128-bit key */
 export function tempEncryption(block: Uint8Array, key: Uint8Array): Uint8Array {
-    const block_temp = new Uint8Array(block.length)
-    block_temp.set(block)
-    for (let i = 0; i < block.length; i++) {
-        block_temp[i] = block_temp[i] ^ key[i]
-    }
-    shiftLeft128(block_temp)
-    return block_temp
+  const block_temp = new Uint8Array(block.length);
+  block_temp.set(block);
+  for (let i = 0; i < block.length; i++) {
+    block_temp[i] = block_temp[i] ^ key[i];
+  }
+  shiftLeft128(block_temp);
+  return block_temp;
 }
 
 /* Decrypts a 128-bit block using a 128-bit key */
 export function tempDecryption(block: Uint8Array, key: Uint8Array): Uint8Array {
-    const block_temp = new Uint8Array(block.length)
-    block_temp.set(block)
-    shiftRight128(block_temp)
-    for (let i = 0; i < block.length; i++) {
-        block_temp[i] = block_temp[i] ^ key[i]
-    }
-    return block_temp
+  const block_temp = new Uint8Array(block.length);
+  block_temp.set(block);
+  shiftRight128(block_temp);
+  for (let i = 0; i < block.length; i++) {
+    block_temp[i] = block_temp[i] ^ key[i];
+  }
+  return block_temp;
 }
 
 /* Converts a Uint8Array to a binary string or a regular string */
-export function uint8ArrayToBinaryOrString(uint8Array: Uint8Array, isBinaryString: Boolean) {
-    if (isBinaryString) {
-        return uint8Array.reduce((acc, byte) => acc + byte.toString(2).padStart(8, '0'), '');
-    } else {
-        return String.fromCharCode.apply(null, Array.from(uint8Array));
-    }
+export function uint8ArrayToBinaryOrString(
+  uint8Array: Uint8Array,
+  isBinaryString: boolean
+) {
+  if (isBinaryString) {
+    return uint8Array.reduce(
+      (acc, byte) => acc + byte.toString(2).padStart(8, "0"),
+      ""
+    );
+  } else {
+    return String.fromCharCode.apply(null, Array.from(uint8Array));
+  }
 }
 
 /* Increment the Counter */
 export function incrementCounter(counter: Uint8Array): void {
-    for (let i = counter.length - 1; i >= 0; i--) {
-        if (counter[i] === 255) { // If maximum value
-            counter[i] = 0; // Reset byte to 0
-        } else {
-            counter[i]++;
-            break; 
-        }
+  for (let i = counter.length - 1; i >= 0; i--) {
+    if (counter[i] === 255) {
+      // If maximum value
+      counter[i] = 0; // Reset byte to 0
+    } else {
+      counter[i]++;
+      break;
     }
+  }
 }
-
-
--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,40 +1,42 @@
 export const stringToByte = (str: string) => {
-  const textEncoder = new TextEncoder()
-  return textEncoder.encode(str)
-}
+  const textEncoder = new TextEncoder();
+  return textEncoder.encode(str);
+};
 
 export const byteToHex = (bytes: Uint8Array) => {
-  return Array.from(bytes)
-  .map((byte) => {
+  return Array.from(bytes).map((byte) => {
     // Convert byte to hexadecimal string with leading zero padding
     const hexString = byte.toString(16).padStart(2, "0");
     return `0x${hexString}`; // Prepend "0x" to each hexadecimal string (e.g 0x6c)
   });
-}
+};
 
 export const hexToByte = (hexArray: ArrayBufferLike) => {
-  return new Uint8Array(hexArray)
-}
+  return new Uint8Array(hexArray);
+};
 
 export const byteToString = (bytes: AllowSharedBufferSource) => {
-  const textDecoder = new TextDecoder()
-  return textDecoder.decode(bytes)
-}
+  const textDecoder = new TextDecoder();
+  return textDecoder.decode(bytes);
+};
 
 export const padBytes = (byteArray: Uint8Array, blockSize: number) => {
   const paddingLength = blockSize - (byteArray.length % blockSize);
-  const padding = new Uint8Array(paddingLength).fill(0);  // pad with 0
+  const padding = new Uint8Array(paddingLength).fill(0); // pad with 0
   return new Uint8Array([...byteArray, ...padding]);
-}
+};
 
-export const splitBytesIntoBlock = (byteArray: Uint8Array, blockSize: number) => {
+export const splitBytesIntoBlock = (
+  byteArray: Uint8Array,
+  blockSize: number
+) => {
   const blocks = [];
   for (let i = 0; i < byteArray.length; i += blockSize) {
     const block = byteArray.slice(i, Math.min(i + blockSize, byteArray.length));
     blocks.push(block);
   }
   return blocks;
-}
+};
 
 export const bytesToBit = (bytes: Uint8Array) => {
   let binaryString = "";
@@ -42,12 +44,12 @@ export const bytesToBit = (bytes: Uint8Array) => {
     binaryString += byte.toString(2).padStart(8, "0") + " ";
   }
   binaryString = binaryString.trim();
-  const binaryArray = binaryString.split(' ')
-  return binaryArray
-}
+  const binaryArray = binaryString.split(" ");
+  return binaryArray;
+};
 
 export const bitToBytes = (bit: string[]) => {
-  const bitString = bit.join('')
+  const bitString = bit.join("");
   const byteArr = [];
   for (let i = 0; i < bitString.length; i += 8) {
     const byteString = bitString.slice(i, i + 8);
@@ -55,4 +57,82 @@ export const bitToBytes = (bit: string[]) => {
     byteArr.push(byteValue);
   }
   return new Uint8Array(byteArr);
-}
+};
+
+export const splitIntoByte = (bits: string) => {
+  const bitArray = [];
+
+  for (let i = 0; i < bits.length; i += 8) {
+    bitArray.push(bits.slice(i, i + 8));
+  }
+  return bitArray;
+};
+
+export const shiftLeft128ByOne = (bits128: Uint8Array) => {
+  const carry = bits128[0] & 0x80 ? 1 : 0;
+  for (let i = 0; i < bits128.length - 1; i++) {
+    bits128[i] = (bits128[i] << 1) | (bits128[i + 1] & 0x80 ? 1 : 0);
+  }
+  bits128[bits128.length - 1] = (bits128[bits128.length - 1] << 1) | carry;
+
+  return bits128;
+};
+
+export const shiftLeft128ByTwo = (bits128: Uint8Array) => {
+  for (let i = 0; i < 2; i++) {
+    bits128 = shiftLeft128ByOne(bits128);
+  }
+  return bits128;
+};
+
+export const shiftRight128ByOne = (bits128: Uint8Array) => {
+  const n = bits128.length;
+  const carry = bits128[n - 1] & 0x01;
+  for (let i = n - 1; i > 0; i--) {
+    bits128[i] = (bits128[i] >> 1) | ((bits128[i - 1] & 0x01) << 7);
+  }
+  bits128[0] = bits128[0] >> 1;
+  bits128[0] |= carry << 7;
+
+  return bits128;
+};
+
+export const shiftRight128ByTwo = (bits128: Uint8Array) => {
+  for (let i = 0; i < 2; i++) {
+    bits128 = shiftRight128ByOne(bits128);
+  }
+  return bits128;
+};
+
+export const expansionPermutation = (bit: string) => {
+  const pbox = [
+    [19, 8, 9, 25, 0, 6, 5, 20, 24, 2, 13, 17, 15, 7, 23, 1],
+    [21, 3, 12, 31, 10, 18, 30, 26, 4, 11, 29, 14, 27, 16, 28, 22],
+    [16, 10, 13, 24, 1, 7, 26, 27, 6, 12, 22, 0, 23, 14, 30, 3],
+    [17, 28, 8, 2, 5, 31, 29, 11, 25, 9, 4, 15, 21, 20, 19, 18]
+  ];
+
+  let permutatedBitFirst = "";
+  let permutatedBitSecond = "";
+
+  for (let i = 0; i < pbox.length; i++) {
+    for (let j = 0; j < pbox[0].length; j++) {
+      if (i < 2) {
+        permutatedBitFirst += bit[pbox[i][j]];
+      } else {
+        permutatedBitSecond += bit[pbox[i][j]];
+      }
+    }
+  }
+
+  return permutatedBitFirst + permutatedBitSecond;
+};
+
+export const splitPlaintextTo128Bit = (plaintext: string) => {
+  const splittedPlaintext = [];
+
+  for (let i = 0; i < plaintext.length; i += 16) {
+    splittedPlaintext.push(plaintext.slice(i, i + 16));
+  }
+  return splittedPlaintext;
+};
